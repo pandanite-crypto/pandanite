e46942db1d626c398c1453df48ddb67cdcca8d98
MrPandaBear <mrpandabear@mrpandabear.org>
2021-11-17 11:29:39 -0800

    remove mining code from server

--- src/tools/server.cpp
+++ src/tools/server.cpp
@@ -12,43 +12,6 @@
 #include "../core/crypto.hpp"
 using namespace std;
 
-void run_mining(PublicWalletAddress wallet, HostManager& hosts) {
-    while(true) {
-        try {
-            std::pair<string,int> bestHost = hosts.getLongestChainHost();
-            if (bestHost.first == "") continue;
-            int bestCount = bestHost.second;
-            string host = bestHost.first;
-            int nextBlock = bestCount + 1;
-            json problem = getMiningProblem(host);
-            string lastHashStr = problem["lastHash"];
-            SHA256Hash lastHash = stringToSHA256(lastHashStr);
-            int challengeSize = problem["challengeSize"];
-            
-            // create fee to our wallet:
-            Transaction fee(wallet, nextBlock);
-            vector<Transaction> transactions;
-            Block newBlock;
-            newBlock.setId(nextBlock);
-            newBlock.addTransaction(fee);
-            for(auto t : problem["transactions"]) {
-                Transaction x(t);
-                newBlock.addTransaction(x);
-            }
-            newBlock.setDifficulty(challengeSize);
-            SHA256Hash hash = newBlock.getHash(lastHash);
-            SHA256Hash solution = mineHash(hash, challengeSize);
-            newBlock.setNonce(solution);
-            // send the solution!
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            cout<<"Submitting solution"<<endl;
-            cout<<submitBlock(host, newBlock)<<endl;
-        } catch (const std::exception& e) {
-            cout<<"failure in mining thread: "<<e.what()<<endl;
-        }
-    }
-}
-
 int main(int argc, char **argv) {
     experimental::filesystem::remove_all(LEDGER_FILE_PATH);
     experimental::filesystem::remove_all(BLOCK_STORE_FILE_PATH);
@@ -63,16 +26,7 @@ int main(int argc, char **argv) {
     }
     HostManager hosts(config);
     RequestManager manager(hosts);
-    
-    bool runMiner = config["runMiner"];
-    if (runMiner) {
-        cout<<"Running miner"<<endl;
-        PublicWalletAddress wallet = walletAddressFromPublicKey(stringToPublicKey(config["minerPublicKey"]));
-        std::thread mining_thread(run_mining, wallet, ref(hosts));
-        mining_thread.detach();
-    }
-    
-
+ 
     uWS::App().get("/block_count", [&manager](auto *res, auto *req) {
         try {
             std::string count = manager.getBlockCount();
