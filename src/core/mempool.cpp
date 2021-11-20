b4aedc2926a7ceab0954bf1ce0161ec70a9cfa02
MrPandaBear <mrpandabear@mrpandabear.org>
2021-11-20 08:03:41 -0800

    fix segfault with queue removal

--- src/core/mempool.cpp
+++ src/core/mempool.cpp
@@ -1,4 +1,6 @@
 #include "mempool.hpp"
+#include <iostream>
+using namespace std;
 
 #define MAX_FUTURE_BLOCKS 5
 
@@ -18,7 +20,7 @@ ExecutionStatus MemPool::addTransaction(Transaction t) {
     ExecutionStatus status;
     if (t.getBlockId() <= this->blockchain.getBlockCount()) {
         return EXPIRED_TRANSACTION;
-    } else if (t.getBlockId() == this->blockchain.getBlockCount() + 1) { // case 1: add to latest block queue
+    } else if (t.getBlockId() == this->blockchain.getBlockCount() + 1) { // case 1: add to latest block queue + verify
         status = this->blockchain.verifyTransaction(t);
         if (status != SUCCESS) {
             return status;
@@ -33,7 +35,7 @@ ExecutionStatus MemPool::addTransaction(Transaction t) {
             this->lock.unlock();
             return status;
         }
-    } else if (t.getBlockId() > this->blockchain.getBlockCount() + 1){ // case 2: add to a future blocks queue
+    } else if (t.getBlockId() > this->blockchain.getBlockCount() + 1){ // case 2: add to a future blocks queue, no verify
         if (t.getBlockId() < this->blockchain.getBlockCount() + MAX_FUTURE_BLOCKS) {   // limit to queueing up to N blocks in future
             this->lock.lock();
             if (this->transactionQueue[t.getBlockId()].size() < MAX_TRANSACTIONS_PER_BLOCK) {
@@ -48,24 +50,30 @@ ExecutionStatus MemPool::addTransaction(Transaction t) {
             return BLOCK_ID_TOO_LARGE;
         }
     }
+    return UNKNOWN_ERROR;
 }
 
 void MemPool::finishBlock(int blockId) {
     this->lock.lock();
     // erase all transactions prior to this block
+    vector<int> toDelete;
     for (auto pair : this->transactionQueue) {
         if (pair.first <= blockId) {
-            this->transactionQueue.erase(pair.first);
+            toDelete.push_back(pair.first);
         }
+    }
+    for (auto blockId : toDelete) {
+        this->transactionQueue.erase(blockId);
     }
     // validate transactions scheduled for the next block if needed:
     if (this->transactionQueue.find(blockId+1) != this->transactionQueue.end()) {
         set<Transaction>& currTransactions =  this->transactionQueue[blockId+1];
-        for(auto it = currTransactions.begin(); it != currTransactions.end(); ++it) {
+        for (auto it = currTransactions.begin(); it != currTransactions.end(); ) {
             ExecutionStatus status = this->blockchain.verifyTransaction(*it);
-            if ((status) != SUCCESS) {
-                // erase invalid transactions
-                currTransactions.erase(it);
+            if (status != SUCCESS) {
+                currTransactions.erase(it++);
+            } else {
+                ++it;
             }
         }
     }
