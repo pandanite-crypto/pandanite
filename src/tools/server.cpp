374af9d3366f5439e28e5e15783dc999af0f500b
MrPandaBear <mrpandabear@mrpandabear.org>
2022-02-17 21:29:16 -0800

    typo

--- src/tools/server.cpp
+++ src/tools/server.cpp
@@ -409,7 +409,7 @@ int main(int argc, char **argv) {
                             Transaction tx(item);
                             response.push_back(manager.addTransaction(tx));
                             // only add a maximum of 100 transactions per request
-                            if (response.size() > 100)) break;
+                            if (response.size() > 100) break;
                         }
                     } else {
                         Transaction tx(parsed);
