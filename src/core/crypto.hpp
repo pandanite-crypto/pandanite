5b54e2d8ce3058bba0d1b7bc407cf09e611ec5d3
MrPandaBear <mrpandabear@mrpandabear.org>
2022-05-29 09:52:01 -0700

    fix default arg

--- src/core/crypto.hpp
+++ src/core/crypto.hpp
@@ -32,5 +32,5 @@ TransactionSignature signWithPrivateKey(string content, PublicKey pubKey, Privat
 TransactionSignature signWithPrivateKey(const char* bytes, size_t len, PublicKey pubKey, PrivateKey privKey);
 bool checkSignature(string content, TransactionSignature signature, PublicKey signingKey);
 bool checkSignature(const char* bytes, size_t len, TransactionSignature signature, PublicKey signingKey);
-SHA256Hash mineHash(SHA256Hash target, unsigned char challengeSize, bool usePufferfish=true);
-bool verifyHash(SHA256Hash& target, SHA256Hash& nonce, unsigned char challengeSize, bool usePufferfish=true);
+SHA256Hash mineHash(SHA256Hash target, unsigned char challengeSize, bool usePufferfish=false);
+bool verifyHash(SHA256Hash& target, SHA256Hash& nonce, unsigned char challengeSize, bool usePufferfish=false);
