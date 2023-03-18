62302e41b0e5083355cc175eff331b693e6488e1
MikeO <10thmountain@zoho.com>
2023-03-18 17:28:49 +0800

    Update logger.hpp

--- src/core/logger.hpp
+++ src/core/logger.hpp
@@ -67,8 +67,7 @@ public:
 };
 
 //list<string> Logger::buffer = list<string>();
-extern list<string> Logger::buffer = list<string>();
-ofstream Logger::file = ofstream();
-std::mutex Logger::console_lock;
-std::mutex Logger::file_mutex;
-std::mutex Logger::buffer_mutex;
+//ofstream Logger::file = ofstream();
+//std::mutex Logger::console_lock;
+//std::mutex Logger::file_mutex;
+//std::mutex Logger::buffer_mutex;
