0ac768e31c3163511da5cc3263311dea8a883ade
MikeO <10thmountain@zoho.com>
2023-03-22 14:08:59 +0800

    update minimum peering version to 0.7.7-beta

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -136,7 +136,7 @@ json getConfig(int argc, char**argv) {
     config["ip"] = customIp;
     config["thread_priority"] = thread_priority;
     config["hostSources"] = hostSources;
-    config["minHostVersion"] = "0.2.4-alpha";
+    config["minHostVersion"] = "0.7.7-beta";
     config["showHeaderStats"] = true;
 
     if (local) {
