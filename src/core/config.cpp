6034e6c0ebd4ae51b2adf6469292d74bb76f93ff
J-Ttt <39967339+J-Ttt@users.noreply.github.com>
2023-04-27 15:23:02 +0200

    Added more hostsources

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -149,6 +149,8 @@ json getConfig(int argc, char**argv) {
         config["hostSources"].push_back("http://173.230.139.86:3002/peers");
         config["hostSources"].push_back("http://178.254.42.138:3000/peers");
         config["hostSources"].push_back("http://65.108.218.239:3000/peers");
+        config["hostSources"].push_back("http://65.108.201.140:3000/peers");
+        config["hostSources"].push_back("http://65.108.201.144:3000/peers");
     }
     return config;
 }
