b29e01cb7d544d0631104744607badddbd546f87
mr-pandabear <94190499+mr-pandabear@users.noreply.github.com>
2023-10-17 08:46:18 -0700

    Fix WLinkNet's bug and roll back. 🖕 @attacker, I will move on to another project.

--- src/server/executor.cpp
+++ src/server/executor.cpp
@@ -220,24 +220,15 @@ ExecutionStatus updateLedger(Transaction t, PublicWalletAddress& miner, Ledger& 
         total -= amt;
 
         if (checkInvalid && total < fees) {
-            if (!isInvalidTransaction(blockId, from) && blockId != 0) {
-                addInvalidTransaction(blockId, from);
-                return BALANCE_TOO_LOW;
-            } 
+            return BALANCE_TOO_LOW;
         }
         if (!withdraw(from, amt, ledger, deltas)) {
-             if (!isInvalidTransaction(blockId, from) && blockId != 0) {
-                addInvalidTransaction(blockId, from);
-                return BALANCE_TOO_LOW;
-            }
+            return BALANCE_TOO_LOW;
         }
         deposit(to, amt, ledger, deltas);
         if (fees > 0) {
             if (!withdraw(from, fees, ledger, deltas)) {
-                 if (!isInvalidTransaction(blockId, from) && blockId != 0) {
-                addInvalidTransaction(blockId, from);
                 return BALANCE_TOO_LOW;
-            }
             }
             deposit(miner, fees, ledger, deltas);
         }
